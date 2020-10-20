272bd5cffaf6ad1f51d5daed9fc9b42a2c265305
code_bug <codebuggg@users.noreply.github.com>
2020-10-20 03:00:53 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,9 @@ Route::get('/sales',[SalesController::class, 'index'])->name('sales');
 Route::get('/sales/create',[SalesController::class, 'create'])->name('newSales');
 Route::post('/sales',[SalesController::class, 'store'])->name('sales.store');
 
-// Route::get('/expense',[expenseController::class, 'index'])->name('expense');
-// Route::get('/expense/create',[expenseController::class, 'create'])->name('newExpense');
-// Route::post('/expense',[expenseController::class, 'store'])->name('expense.store');
+Route::get('/expense',[expenseController::class, 'index'])->name('expense');
+Route::get('/expense/create',[expenseController::class, 'create'])->name('newExpense');
+Route::post('/expense',[expenseController::class, 'store'])->name('expense.store');
 
 
 Route::get('/expense', function () {
@@ -60,10 +60,10 @@ Route::get('/messages/newMessages', function () {
     return view('logistics.newMessages');
 })->name('newMesages');
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
