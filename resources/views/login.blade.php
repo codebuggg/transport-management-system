cc53ac6cfa5c3a922e006c71486ee7535b366a2b
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-11-03 19:04:20 +0000

    Fix login layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,28 @@
-@extends('layouts.app')
-    <body>
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Login</title>
+        <!-- Styles -->
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <style>
+            .primary-text-color{
+            color:#07223f;
+            }
+            .primary-background-color{
+                background-color: #07223f;
+            }
+        </style>
+    </head>
+    <body class="bg-gray-200">
         <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
 
             <div class="max-w-md w-full">
             <div>
                 <img class="mx-auto h-48 w-auto" src={{asset('asset/images/logos.png')}} alt="logo">
-                <h2 class=" text-center text-3xl leading-9 font-extrabold text-gray-900">
+                <h2 class=" text-center text-3xl leading-9 font-extrabold primary-text-color">
                 Sign in
                 </h2>
 
@@ -44,7 +60,7 @@
 
                 @if (Route::has('password.request'))
                     <div class="text-sm leading-5">
-                        <a href="{{ route('password.request') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                        <a href="{{ route('password.request') }}" class="font-medium primary-text-color focus:outline-none focus:underline transition ease-in-out duration-150">
                             {{ __('Forgot Your Password?') }}
                         </a>
                     </div>
@@ -52,9 +68,9 @@
                 </div>
 
                 <div class="mt-6">
-                <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-black-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white primary-background-color focus:outline-none focus:border-black-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
                     <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                    <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                    <svg class="h-5 w-5 text-white transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                     </svg>
                     </span>
